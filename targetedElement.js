ed90d8e4f7d71c203641650ebc8cb07c3bcdb9e2
amritesh-bhu <amriteshbhu438@gmail.com>
2025-08-21 09:47:59 +0530

    finding sum of three element in array should be equal to target!!

---
+++ targetedElement.js
@@ -0,0 +1,15 @@
+function targetElement(arr, target){
+  
+    for (let i = 0; i< arr.length - 2; i++){
+        for (let j = i+1; j < arr.length-1; j++){
+            for (let k = j+1; k < arr.length; k++){
+                if(arr[i]+arr[j]+arr[k] === target){
+                    return true
+                }
+            }
+        }
+    }
+    return false
+  }
+  
+  console.log(targetElement([1, 4, 45, 6, 10, 8], 100)); 
